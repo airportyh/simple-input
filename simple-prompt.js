b4747575fa542f2315d26a3609b97b92f2e2571e
Toby Ho <airportyh@gmail.com>
2019-05-22 14:40:41 -0400

    Update by Drive By.

--- simple-prompt.js
+++ simple-prompt.js
@@ -1,5 +1,7 @@
+const readline = require('readline');
+
 module.exports = function prompt(message) {
-    const readline = require('readline');
+    
 
     const rl = readline.createInterface({
     input: process.stdin,
